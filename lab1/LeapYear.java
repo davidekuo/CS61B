84aaa5142beb2de12521674e33700f18885bd6df
David Kuo <dkuo@ucsd.edu>
2020-02-21 21:33:29 -0800

    completed Leap Year

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -14,6 +14,11 @@ public class LeapYear {
         }
     }
 
+    /** Returns TRUE if year is a leap year, FALSE otherwise */
+    public static boolean isLeapYear(int year) {
+        return year % 400 == 0 || (year % 4 == 0 && year % 100 != 0);
+    }
+
     /** Must be provided an integer as a command line argument ARGS. */
     public static void main(String[] args) {
         if (args.length < 1) {
