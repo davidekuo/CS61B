e65d92098c9b20281eb768a6424f8a6282273b0d
David Kuo <dkuo@ucsd.edu>
2020-04-01 13:49:12 -0700

    Completed Lab 3

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -9,3 +9,12 @@ public class HorribleSteve {
         System.out.println("i is " + i);
     }
 }
+
+/** The bug here is that Flik is comparing Integers (reference type) instead of ints (primitive type).
+    As such, what is being compared are addresses in memory as opposed to integer values.
+    When the addresses are different, even if integer values are the same, isSameNumber returns false.
+    This can be fixed by changing
+        public static boolean isSameNumber(Integer a, Integer b)
+    to
+        public static boolean isSameNumber(int a, int b)
+ */
