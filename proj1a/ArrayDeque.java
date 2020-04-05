4fc47508009b13eb5a09c131b36f85c1a9747035
David Kuo <dkuo@ucsd.edu>
2020-04-05 12:25:04 -0700

    Completed and tested Project 1A: implemented and tested Array Deque with resizing

--- proj1a/ArrayDeque.java
+++ proj1a/ArrayDeque.java
@@ -6,7 +6,7 @@ public class ArrayDeque<T> {
     public ArrayDeque() {
         arr = (T []) new Object[8];
         size = 0;
-        first = 7;
+        first = 4;
         last = movePrev(first);
         RFACTOR = 2;
     }
@@ -61,21 +61,32 @@ public class ArrayDeque<T> {
     }
 
     public void addFirst(T item) {
+        if (size == arr.length) {
+            resize(arr.length * RFACTOR);
+        }
         first = movePrev(first);
         arr[first] = item;
         size++;
     }
 
     public void addLast(T item) {
+        if (size == arr.length) {
+            resize(arr.length * RFACTOR);
+        }
         last = moveNext(last);
         arr[last] = item;
         size++;
+    }
 
     public T removeFirst() {
         if (size == 0) {
             return null;
         } else {
+            if (arr.length >= 16 && size * 4 <= arr.length) {
+                resize(arr.length / 2);
+            }
             T value = arr[first];
+            arr[first] = null;
             first = moveNext(first);
             size--;
             return value;
@@ -86,12 +97,44 @@ public class ArrayDeque<T> {
         if (size == 0) {
             return null;
         } else {
+            if (arr.length >= 16 && size * 4 <= arr.length) {
+                resize(arr.length / 2);
+            }
             T value = arr[last];
+            arr[last] = null;
             last = movePrev(last);
             size--;
             return value;
         }
     }
+
+    public T[] extract() {
+        // extracts the deque from Array straightened and without extra elements
+        // Ex. [4 5 null null 1 2 3] -> [1 2 3 4 5]
+        T[] extracted = (T []) new Object[size];
+        if (size == 0) {
+            return null;
+        } else if (size == 1) {
+            extracted[0] = arr[first];
+        } else if (first < last) {
+            System.arraycopy(arr, first, extracted, 0, size);
+            // tested at https://tinyurl.com/yx7wtycc (Java Visualizer)
+        } else if (first > last) {
+            System.arraycopy(arr, first, extracted, 0, arr.length-first);
+            System.arraycopy(arr, 0, extracted, arr.length-first, last+1);
+            // tested at https://tinyurl.com/uu8cc8q (Java Visualizer)
+        }
+        return extracted;
+    }
+
+    public void resize(int newSize) {
+        T[] resized = (T []) new Object[newSize];
+        T[] extracted = extract();
+        first = newSize/4;
+        last = first+size-1;
+        System.arraycopy(extracted, 0, resized, first, size);
+        arr = resized;
+    } // tested at https://tinyurl.com/vqco8af (Java Visualizer)
 
     public static void main(String[] args) {
         ArrayDeque<Integer> test = new ArrayDeque<>();
@@ -101,48 +144,38 @@ public class ArrayDeque<T> {
         System.out.print("Testing get(): \nExpect null - ");
         System.out.print(test.get(0) + "\n");
 
-        test.addFirst(5);
-        test.addLast(6);
-        test.addFirst(4);
-        test.addFirst(3);
-        test.addFirst(2);
-        test.addLast(7);
-        test.addFirst(1);
-        test.addFirst(0);
-        System.out.print("Testing addFirst() & addLast(): \nExpect 0 1 2 3 4 5 6 7 - ");
+        for (Integer i = 0; i < 11; i++) {
+            test.addLast(i);
+        }
+        System.out.print("Testing addLast(): \nExpect 0 1 2 3 4 5 6 7 8 9 10 - ");
         test.printDeque();
         System.out.println();
 
-        System.out.print("Testing get(): \nExpect 3 - ");
+        for (Integer i = 1; i < 11; i++) {
+            test.addFirst(i);
+        }
+        System.out.print("Testing addFirst(): \nExpect 10 9 8 7 6 5 4 3 2 1 0 1 2 3 4 5 6 7 8 9 10 - ");
+        test.printDeque();
+        System.out.println();
+
+        System.out.print("Testing get(3): \nExpect 7 - ");
         System.out.print(test.get(3) + "\n");
 
-        test.removeLast();
-        test.addFirst(1);
-        test.removeLast();
-        test.addFirst(2);
-        test.removeLast();
-        test.addFirst(3);
-        test.removeLast();
-        test.addFirst(4);
-        test.addFirst(5);
-        test.removeLast();
-        test.removeLast();
-        test.addFirst(6);
-        test.removeLast();
-        test.addFirst(7);
-        System.out.print("Testing removeFirst() & removeLast(): \nExpect 7 6 5 4 3 2 1 0 - ");
-        test.printDeque();
-        System.out.println();
-
-        test.removeFirst();
-        test.removeLast();
-        test.removeLast();
-        test.removeFirst();
-        System.out.print("Testing removeFirst() & removeLast(): \nExpect 5 4 3 2 - ");
+        for (Integer i = 0; i < 15; i++) {
+            test.removeFirst();
+        }
+        System.out.print("Testing removeFirst(): \nExpect 5 6 7 8 9 10 - ");
         test.printDeque();
         System.out.println();
 
         System.out.print("Testing get(): \nExpect null - ");
         System.out.print(test.get(10) + "\n");
+
+        for (Integer i = 0; i < 5; i++) {
+            test.removeLast();
+        }
+        System.out.print("Testing removeLast(): \nExpect 5 - ");
+        test.printDeque();
+        System.out.println();
     }
 }
