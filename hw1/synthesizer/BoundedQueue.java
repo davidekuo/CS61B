9c11306eb8cb8dbe471a77f84a3f69f8c03ba857
David Kuo <dkuo@ucsd.edu>
2020-04-28 17:33:25 -0700

    Completed HW1 except for optional GuitarHero, passed local tests and style checker

---
+++ hw1/synthesizer/BoundedQueue.java
@@ -0,0 +1,22 @@
+package synthesizer;
+
+import java.util.Iterator;
+
+public interface BoundedQueue<T> extends Iterable<T> {
+
+    int capacity();         // return size of buffer
+    int fillCount();         // return number of items currently in the buffer
+    void enqueue(T x);      // add item x to the end
+    T dequeue();            // delete and return item from the front
+    T peek();               // return (but do not delete) item from the front
+
+    Iterator<T> iterator(); // required abstract method for Iterable interface
+
+    default boolean isEmpty() {
+        return fillCount() == 0;
+    }
+
+    default boolean isFull() {
+        return fillCount() == capacity();
+    }
+}
