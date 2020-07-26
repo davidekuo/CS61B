f5d736f9fe6366fbe43431de020e285096ef18c4
David Kuo <dkuo@ucsd.edu>
2020-07-25 17:15:37 -0700

    HW2: revised Percolation datatype with optimization hints to meet performance requirements and avoid backwash

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -1,7 +1,6 @@
 package hw2;
 
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
-import java.util.HashSet;
 
 public class Percolation {
 
@@ -13,15 +12,8 @@ public class Percolation {
     private boolean[][] openArray; // tracks which array elements have been opened
     // Hopefully more memory efficient than adding 3rd dimension to indexArray b/c boolean instead of int
 
-    private WeightedQuickUnionUF disjointSet;
-
-    private HashSet<Integer> openTop; // tracks disjoint set parents of open elements in top row (row = 0)
-    // any element with one of these as their parent is full b/c it is connected to an open top row element
-
-    private HashSet<Integer> openBottom; // tracks disjoint set parents of open elements in bottom row (row = N - 1)
-    // percolates() is true iff openBottom and openTop intersect i.e. an open top element and open bottom element
-    // are in the same set and thus connected to one another
-
+    private WeightedQuickUnionUF fullSet; // disjoint set for tracking whether each element is full
+    private WeightedQuickUnionUF percolateSet; // disjoint set for tracking whether percolation has occurred
 
     // create N-by-N grid, with all sites initially blocked
     public Percolation(int N) {
@@ -31,14 +23,22 @@ public class Percolation {
 
         this.N = N;
         numOpenSites = 0;
-        openTop = new HashSet<>();
-        openBottom = new HashSet<>();
-        disjointSet = new WeightedQuickUnionUF(N*N);
+
+        fullSet = new WeightedQuickUnionUF(N * N + 1);
+        // + 1 because index 0 will be a virtual "top" element which we will connect to each element in the top row
+        // indices 1 ... N*N correspond to [row][col] in index array which will be initialized below
+        // this allows us to implement isFull() with connected(index, 0)
+
+        percolateSet = new WeightedQuickUnionUF(N * N + 2);
+        // + 2 because index 0 will be a virtual "top" element to be connected to each open element in the top row
+        // and index N * N + 1 will be a virtual "bottom" element to be connect to each open element in the bottom row
+        // as above, indices 1 ... N*N correspond to [row][col] in index array which will be initialized below
+        // this allows us to implement percolate() with connected(0, N * N + 1)
 
         // initialize reference array
         indexArray = new int[N][N];
         openArray = new boolean[N][N];
-        int index = 0;
+        int index = 1; // start at 1 because index 0 is reserved for virtual "top" element
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
                 indexArray[i][j] = index;
@@ -61,38 +61,34 @@ public class Percolation {
 
         // connect/union new open element with its legal & open neighbors (avoid index out of bounds)
         if (row - 1 >= 0 && isOpen(row - 1, col)) {
-            disjointSet.union(indexArray[row][col], indexArray[row - 1][col]);
+            fullSet.union(indexArray[row][col], indexArray[row - 1][col]);
+            percolateSet.union(indexArray[row][col], indexArray[row - 1][col]);
         }
         if (row + 1 < N && isOpen(row + 1, col)) {
-            disjointSet.union(indexArray[row][col], indexArray[row + 1][col]);
+            fullSet.union(indexArray[row][col], indexArray[row + 1][col]);
+            percolateSet.union(indexArray[row][col], indexArray[row + 1][col]);
         }
         if (col - 1 >= 0 && isOpen(row, col - 1)) {
-            disjointSet.union(indexArray[row][col], indexArray[row][col - 1]);
+            fullSet.union(indexArray[row][col], indexArray[row][col - 1]);
+            percolateSet.union(indexArray[row][col], indexArray[row][col - 1]);
         }
         if (col + 1 < N && isOpen(row, col + 1)) {
-            disjointSet.union(indexArray[row][col], indexArray[row][col + 1]);
-        }
-
-        // after new connect/union operations, update parents of open elements in top row as these may have changed
-        // important to keep this up-to-date for isFull() and percolate() methods
-        if (!openTop.isEmpty()) {
-            int[] oldparents = openTop.stream().mapToInt(Integer::intValue).toArray();
-            for (int i : oldparents) {
-                openTop.add(disjointSet.find(i));
-            }
+            fullSet.union(indexArray[row][col], indexArray[row][col + 1]);
+            percolateSet.union(indexArray[row][col], indexArray[row][col + 1]);
         }
 
         openArray[row][col] = true;
         numOpenSites += 1;
 
-        // if new open element is in the top row, add its disjoint set parent to openTop
+        // if new open element is in the top row, connect/union to virtual "top" element (index 0)
         if (row == 0) {
-            openTop.add(disjointSet.find(indexArray[row][col]));
+            fullSet.union(indexArray[row][col], 0);
+            percolateSet.union(indexArray[row][col], 0);
         }
 
-        // if new open element is in the bottom row, add its disjoint set parent to openBottom
+        // if new open element is in the bottom row, connect/union to virtual "bottom" element (index N * N + 1)
         if (row == N - 1) {
-            openBottom.add(disjointSet.find(indexArray[row][col]));
+            percolateSet.union(indexArray[row][col], N * N + 1);
         }
     }
 
@@ -101,7 +97,6 @@ public class Percolation {
         if (row < 0 || row >= N || col < 0 || col >= N) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-
         return openArray[row][col];
     }
 
@@ -110,10 +105,9 @@ public class Percolation {
         if (row < 0 || row >= N || col < 0 || col >= N) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-
-        int parent = disjointSet.find(indexArray[row][col]);
-
-        return openTop.contains(parent);
+        // a site is full iff it is connected to one of the top row elements
+        // and by extension, the virtual "top" element (index 0)
+        return fullSet.connected(indexArray[row][col], 0);
     }
 
     // number of open sites
@@ -123,16 +117,9 @@ public class Percolation {
 
     // does the system percolate?
     public boolean percolates() {
-        if (!openBottom.isEmpty()) {
-            for (int i : openBottom) {
-                // check up-to-date parents of open elements in bottom row
-                // against parents of open elements in top row
-                if (openTop.contains(disjointSet.find(i))) {
-                    return true;
-                }
-            }
-        }
-        return false;
+        // percolation only occurs if a top row element (connected to the virtual "top" element [index 0])
+        // is connected to a bottom row element (connected to the virtual "bottom" element [index N * N + 1])
+        return percolateSet.connected(0, N * N + 1);
     }
 
     // use for unit testing (not required)
