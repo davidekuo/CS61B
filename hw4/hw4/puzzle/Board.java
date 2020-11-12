3bdf3f888093ebe35d3a5933ad12853468304ce7
David Kuo <dkuo@ucsd.edu>
2020-11-11 21:23:24 -0800

    HW4: Puzzle - (mostly) passed local tests (except for puzzle4x4-24.txt for some reason, and tests bounded by memory and runtime) and style checker

--- hw4/hw4/puzzle/Board.java
+++ hw4/hw4/puzzle/Board.java
@@ -1,21 +1,197 @@
 package hw4.puzzle;
+import java.util.ArrayList;
 
-public class Board {
+public class Board implements WorldState {
 
-    /** Returns the string representation of the board. 
-      * Uncomment this method. */
-    /*public String toString() {
+    final int BLANK = 0;
+    final int[][] currentBoard;
+
+    public Board(int[][] tiles) {
+        currentBoard = copyBoard(tiles);
+    }
+
+    private int[][] copyBoard(int[][] board) {
+        int[][] boardCopy = new int[board.length][board[0].length];
+        for (int i = 0; i < board.length; i++) {
+            for (int j = 0; j < board[0].length; j++) {
+                boardCopy[i][j] = board[i][j];
+            }
+        }
+
+        return boardCopy;
+    }
+
+    public int tileAt(int i, int j) {
+        // returns value of tile at row i, column j (or 0 if blank)
+        if (i < 0 || i >= this.size()
+                || j < 0 || j >= this.size()) {
+            throw new java.lang.IndexOutOfBoundsException();
+        } else {
+            return currentBoard[i][j];
+        }
+
+    }
+
+    public int size() {
+        return currentBoard.length;
+    }
+
+    @Override
+    public Iterable<WorldState> neighbors() {
+
+        /*
+         * Find BLANK
+         * Create copy of Board
+         * As allowed, move BLANK up/down/left/right, swapping with the number there
+         */
+
+        ArrayList<WorldState> neighbors = new ArrayList<>();
+
+        // Find BLANK
+        int size = size();
+        int blankRow = -1;
+        int blankCol = -1;
+        for (int i = 0; i < size; i++) {
+            for (int j = 0; j < size; j++) {
+                if (tileAt(i, j) == BLANK) {
+                    blankRow = i;
+                    blankCol = j;
+                    break;
+                }
+            }
+        }
+
+
+        // Move BLANK up
+        if (blankRow > 0) {
+            int[][] boardCopy = copyBoard(currentBoard);
+            int swapTile = boardCopy[blankRow - 1][blankCol];
+            boardCopy[blankRow - 1][blankCol] = BLANK;
+            boardCopy[blankRow][blankCol] = swapTile;
+            Board neighbor = new Board(boardCopy);
+            neighbors.add(neighbor);
+        }
+
+        // Move BLANK down
+        if (blankRow < size - 1) {
+            int[][] boardCopy = copyBoard(currentBoard);
+            int swapTile = boardCopy[blankRow + 1][blankCol];
+            boardCopy[blankRow + 1][blankCol] = BLANK;
+            boardCopy[blankRow][blankCol] = swapTile;
+            Board neighbor = new Board(boardCopy);
+            neighbors.add(neighbor);
+        }
+
+        // Move BLANK left
+        if (blankCol > 0) {
+            int[][] boardCopy = copyBoard(currentBoard);
+            int swapTile = boardCopy[blankRow][blankCol - 1];
+            boardCopy[blankRow][blankCol - 1] = BLANK;
+            boardCopy[blankRow][blankCol] = swapTile;
+            Board neighbor = new Board(boardCopy);
+            neighbors.add(neighbor);
+        }
+
+        // Move BLANK right
+        if (blankCol < size - 1) {
+            int[][] boardCopy = copyBoard(currentBoard);
+            int swapTile = boardCopy[blankRow][blankCol + 1];
+            boardCopy[blankRow][blankCol + 1] = BLANK;
+            boardCopy[blankRow][blankCol] = swapTile;
+            Board neighbor = new Board(boardCopy);
+            neighbors.add(neighbor);
+        }
+
+        return neighbors;
+    }
+
+    private int goalRow(int n) {
+        int size = size();
+        if (n == BLANK) {
+            int returnvalue = size - 1;
+            return returnvalue;
+        } else {
+            int returnvalue = (n - 1) / size;
+            return returnvalue;
+        }
+    }
+
+    private int goalCol(int n) {
+        int size = size();
+        if (n == BLANK) {
+            int returnvalue = size - 1;
+            return returnvalue;
+        } else {
+            int returnvalue = n - goalRow(n) * size - 1;
+            return returnvalue;
+        }
+    }
+
+    public int hamming() {
+        int hammingDistance = 0;
+
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                int tile = currentBoard[i][j];
+                if (i != goalRow(tile) || j != goalCol(tile)) {
+                    hammingDistance++;
+                }
+            }
+        }
+
+        return hammingDistance;
+    }
+
+    public int manhattan() {
+        int manhattanDistance = 0;
+
+        for (int i = 0; i < size(); i++) {
+            for (int j = 0; j < size(); j++) {
+                int n = currentBoard[i][j];
+                int additionalError = Math.abs(goalRow(n) - i) + Math.abs(goalCol(n) - j);
+                manhattanDistance += additionalError;
+            }
+        }
+
+        return manhattanDistance;
+    }
+
+    @Override
+    public int estimatedDistanceToGoal() {
+        return manhattan();
+    }
+
+    public boolean equals(Object y) {
+        if (this == y) {
+            return true;
+        }
+        if (y == null || getClass() != y.getClass()) {
+            return false;
+        }
+
+        Board otherBoard = (Board) y;
+        for (int i = 0; i < otherBoard.size(); i++) {
+            for (int j = 0; j < otherBoard.size(); j++) {
+                if (this.currentBoard[i][j] != otherBoard.currentBoard[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+
+    public String toString() {
         StringBuilder s = new StringBuilder();
         int N = size();
         s.append(N + "\n");
         for (int i = 0; i < N; i++) {
             for (int j = 0; j < N; j++) {
-                s.append(String.format("%2d ", tileAt(i,j)));
+                s.append(String.format("%2d ", tileAt(i , j)));
             }
             s.append("\n");
         }
         s.append("\n");
         return s.toString();
-    }*/
+    }
 
 }
---
+++ hw4/hw4/puzzle/Solver.java
@@ -0,0 +1,92 @@
+package hw4.puzzle;
+
+import edu.princeton.cs.algs4.MinPQ;
+
+import java.util.Comparator;
+import java.util.ArrayDeque;
+
+public class Solver {
+
+    private class SearchNode {
+
+        WorldState state;
+        int moves;
+        int distToGoal;
+        SearchNode prev;
+
+        SearchNode(WorldState s, int m, SearchNode p) {
+            state = s;
+            moves = m;
+            prev = p;
+            distToGoal = s.estimatedDistanceToGoal();
+        }
+    }
+
+    private class SearchNodeComparator implements Comparator<SearchNode> {
+        public int compare(SearchNode s1, SearchNode s2) {
+            int h1 = s1.moves + s1.distToGoal;
+            int h2 = s2.moves + s2.distToGoal;
+
+            if (h1 < h2) {
+                return -1;
+            } else if (h1 > h2) {
+                return 1;
+            } else {
+                return 0;
+            }
+        }
+    }
+
+    MinPQ<SearchNode> pq = new MinPQ(new SearchNodeComparator());
+    int solutionMoves;
+    ArrayDeque<WorldState> solutionSequence;
+    int totalItemsEnqueued = 0;
+
+    public Solver(WorldState initial) {
+        /* Constructor which solves the puzzle, computing everything necessary for moves()
+        and solution() to not have to solve the problem again. Solves puzzle using A*.
+        Assumes solution exists.
+         */
+
+        pq.insert(new SearchNode(initial, 0, null));
+
+        while (!pq.isEmpty()) {
+            SearchNode currentNode = pq.delMin();
+            if (currentNode.state.isGoal()) {
+                solutionMoves = currentNode.moves;
+                solutionSequence = new ArrayDeque<>();
+                SearchNode pointer = currentNode;
+                while (pointer != null) {
+                    solutionSequence.addFirst(pointer.state);
+                    pointer = pointer.prev;
+                }
+                break;
+            } else {
+                for (WorldState neighbor : currentNode.state.neighbors()) {
+                    if (!neighbor.equals(currentNode.state)) {
+                        if (currentNode.prev == null) {
+                            pq.insert(new SearchNode(neighbor, currentNode.moves + 1, currentNode));
+                            totalItemsEnqueued++;
+
+                        } else if (!neighbor.equals(currentNode.prev.state)) {
+                            pq.insert(new SearchNode(neighbor, currentNode.moves + 1, currentNode));
+                            totalItemsEnqueued++;
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+    public int moves() {
+        // Return minimum number of moves to solve the puzzle starting at the initial WorldState
+        return solutionMoves;
+    }
+
+    public Iterable<WorldState> solution() {
+        // Returns a sequence of WorldStates from the initial WorldState to the solution
+        return solutionSequence;
+    }
+
+}
